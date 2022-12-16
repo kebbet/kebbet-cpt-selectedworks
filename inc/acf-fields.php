f9610c3b398aaa04d0a584b51d92876d6ac4f2a8
Erik <11491369+kebbet@users.noreply.github.com>
2022-12-16 09:01:25 +0100

    ACF: Hook field registration to ´acf/init´.

--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -8,56 +8,70 @@
 namespace cpt\kebbet\selectedworks\acf_fields;
 
 if ( class_exists( 'ACF' ) ) :
-acf_add_local_field_group(
-	array(
-		'key'                   => 'group_63076c2d54dff',
-		'title'                 => __( 'Relation to project', 'kebbet-cpt-selectedworks' ),
-		'fields'                => array(
-			array(
-				'key'               => 'field_63076c382db1a',
-				'label'             => 'Relation',
-				'name'              => 'project_relation',
-				'type'              => 'relationship',
-				'instructions'      => '',
-				'required'          => 0,
-				'conditional_logic' => 0,
-				'wrapper'           => array(
-					'width' => '',
-					'class' => '',
-					'id'    => '',
-				),
-				'post_type'         => array(
-					0 => 'project',
-				),
-				'taxonomy'          => '',
-				'filters'           => array(
-					0 => 'search',
-				),
-				'elements'          => '',
-				'min'               => '',
-				'max'               => 1,
-				'return_format'     => 'object',
-			),
-		),
-		'location'              => array(
-			array(
-				array(
-					'param'    => 'post_type',
-					'operator' => '==',
-					'value'    => 'selected-works',
-				),
-			),
-		),
-		'menu_order'            => 0,
-		'position'              => 'normal',
-		'style'                 => 'default',
-		'label_placement'       => 'top',
-		'instruction_placement' => 'label',
-		'hide_on_screen'        => '',
-		'active'                => true,
-		'description'           => '',
-		'show_in_rest'          => 0,
-	)
-);
+	/**
+	 * Add field group for post type meta data
+	 *
+	 * @return void
+	 */
+	function add_group() {
+		acf_add_local_field_group( options() );
+	}
+	add_action( 'acf/init', __NAMESPACE__ . '\add_group' );
 
+	/**
+	 * Return the options for the field group
+	 *
+	 * @return array
+	 */
+	function options() {
+		return array(
+			'key'                   => 'group_63076c2d54dff',
+			'title'                 => __( 'Relation to project', 'kebbet-cpt-selectedworks' ),
+			'fields'                => array(
+				array(
+					'key'               => 'field_63076c382db1a',
+					'label'             => 'Relation',
+					'name'              => 'project_relation',
+					'type'              => 'relationship',
+					'instructions'      => '',
+					'required'          => 0,
+					'conditional_logic' => 0,
+					'wrapper'           => array(
+						'width' => '',
+						'class' => '',
+						'id'    => '',
+					),
+					'post_type'         => array(
+						0 => 'project',
+					),
+					'taxonomy'          => '',
+					'filters'           => array(
+						0 => 'search',
+					),
+					'elements'          => '',
+					'min'               => '',
+					'max'               => 1,
+					'return_format'     => 'object',
+				),
+			),
+			'location'              => array(
+				array(
+					array(
+						'param'    => 'post_type',
+						'operator' => '==',
+						'value'    => 'selected-works',
+					),
+				),
+			),
+			'menu_order'            => 0,
+			'position'              => 'normal',
+			'style'                 => 'default',
+			'label_placement'       => 'top',
+			'instruction_placement' => 'label',
+			'hide_on_screen'        => '',
+			'active'                => true,
+			'description'           => '',
+			'show_in_rest'          => 0,
+		);
+	};
 endif;
